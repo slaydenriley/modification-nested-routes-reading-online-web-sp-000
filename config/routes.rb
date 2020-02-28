4a5c8d513d8a171c4b229ac956fcb1fcf4f1b220
Riley Slayden <slaydenriley@gmail.com>
2020-02-28 18:56:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :authors, only: [:show, :index] do
-  resources :posts, only: [:show, :index, :new]
-end
-resources :posts
+    resources :posts, only: [:show, :index, :new]
+  end
+  resources :posts
 end
